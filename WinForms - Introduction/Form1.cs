12634e21a4aa0a68afa70fdb58c60124d0111a05
Sebastia n <KamashiVentures@gmail.com>
2025-02-13 15:27:02 +0100

    Winforms - Introduction

---
+++ WinForms - Introduction/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms___Introduction
+{
+    public partial class Form1: Form
+    {
+        int counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            counter++;
+            label1.Text = counter.ToString();
+        }
+    }
+}
