b327b2189a1fc763823f6f1274bca7723696d12a
Sebastia n <KamashiVentures@gmail.com>
2025-02-17 17:26:43 +0100

    WinForms - Toolbox

---
+++ WinForms - Toolbox/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace WinForms___Toolbox
+{
+    public partial class Form1: Form
+    {
+        List<Control> ctList = new List<Control>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "bnt1";
+            label1.BackColor = Color.Red;
+
+            AddToList(ctList);
+            ChangeMyControls();
+            label1.Text = DisplayListNames(ctList);
+        }
+
+        void AddToList(List<Control> myList)
+        {
+            foreach (Control c in groupBox1.Controls)
+            {
+                myList.Add(c);
+            }
+        }
+
+        string DisplayListNames(List<Control> myList)
+        {
+            string listNames = "";
+            foreach (Control c in myList)
+            {
+                listNames += c.Name + " ";
+            }
+            return listNames;
+        }
+
+        void ChangeMyControls()
+        {
+            foreach(Control c in groupBox1.Controls)
+            {
+                c.BackColor = Color.Green;
+            }
+        }
+    }
+}
---
+++ WinForms - Toolbox/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WinForms - Toolbox")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinForms - Toolbox")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9dc86dab-b02b-48b9-9ee5-c654c8e4b9ce")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
