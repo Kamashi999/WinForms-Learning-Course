0deea72ee0d079728c123377538b67366579ea0e
Sebastia n <KamashiVentures@gmail.com>
2025-02-20 19:23:29 +0100

    Sandbox

--- WinForms - Sandbox #1/Form1.cs
+++ WinForms - Sandbox #1/Form1.cs
@@ -12,9 +12,37 @@ namespace WinForms___Sandbox__1
 {
     public partial class Form1: Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        void TransferMessage()
+        {
+            label1.Text = textBox1.Text;
+        }
+
+        void LabelNotNull()
+        {
+            if (label1.Text.Length == 0) {
+                label1.Text = "Write something!";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            TransferMessage();
+            Button btn = sender as Button;
+            if (checkBox1.Checked == true)
+            {
+                label1.BackColor = btn.BackColor;
+            } else
+            {
+                label1.BackColor = default;
+            }
+            LabelNotNull();
         }
     }
 }
