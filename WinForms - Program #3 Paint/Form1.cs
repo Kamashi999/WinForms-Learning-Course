43bb79550511d0da16aec9a5659e883f735097ab
Sebastia n <KamashiVentures@gmail.com>
2025-02-20 18:30:29 +0100

    WinForms - Program Paint

---
+++ WinForms - Program #3 Paint/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms___Program__3_Paint
+{
+    public partial class Form1: Form
+    {
+        Graphics gr;
+        Pen pen;
+        bool isPaiting;
+        Point startPos;
+        Point endPos;
+        ColorDialog colorDialog;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            gr = panel1.CreateGraphics();
+            pen = new Pen(Color.Black, 10);
+            gr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            colorDialog = new ColorDialog();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isPaiting = true;
+            startPos = e.Location;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isPaiting = false;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPaiting == true)
+            {
+                endPos = e.Location;
+                gr.DrawLine(pen, startPos, endPos);
+                startPos = endPos;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                pen.Color = colorDialog.Color;
+            }
+        }
+    }
+}
---
+++ WinForms - Program #3 Paint/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WinForms - Program #3 Paint")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinForms - Program #3 Paint")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2f29e3ab-c8f6-4b4b-b372-6ea3845d7cff")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
