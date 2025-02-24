45cce3f35f6b5007be901b7e156690445018297c
Sebastia n <KamashiVentures@gmail.com>
2025-02-24 20:38:50 +0100

    Button adjusts

--- WinForms - Program #4 ToDoList/Form1.Designer.cs
+++ WinForms - Program #4 ToDoList/Form1.Designer.cs
@@ -35,11 +35,13 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Controls.Add(this.richTextBox1);
             this.groupBox1.Controls.Add(this.button4);
             this.groupBox1.Controls.Add(this.button3);
@@ -74,16 +76,17 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(0, 303);
+            this.button2.Location = new System.Drawing.Point(0, 291);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(200, 45);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Reset";
+            this.button2.Text = "Reset at";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(0, 354);
+            this.button3.Location = new System.Drawing.Point(0, 342);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(200, 45);
             this.button3.TabIndex = 3;
@@ -92,12 +95,13 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(0, 405);
+            this.button4.Location = new System.Drawing.Point(0, 393);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(200, 45);
             this.button4.TabIndex = 4;
             this.button4.Text = "Exit";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // richTextBox1
             // 
@@ -106,6 +110,13 @@
             this.richTextBox1.Size = new System.Drawing.Size(188, 104);
             this.richTextBox1.TabIndex = 5;
             this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(122, 304);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(17, 20);
+            this.textBox1.TabIndex = 6;
             // 
             // Form1
             // 
@@ -117,6 +128,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -130,6 +142,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- WinForms - Program #4 ToDoList/Form1.cs
+++ WinForms - Program #4 ToDoList/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WinForms___Program__4_ToDoList
 {
@@ -19,7 +20,29 @@ namespace WinForms___Program__4_ToDoList
 
         private void button1_Click(object sender, EventArgs e)
         {
-            checkedListBox1.Text += "task";
+            if (!string.IsNullOrWhiteSpace(richTextBox1.Text))
+            {
+                checkedListBox1.Items.Add(richTextBox1.Text);
+                richTextBox1.Clear();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                checkedListBox1.Items.RemoveAt(int.Parse(textBox1.Text) - 1);
+                textBox1.Clear();
+            } else
+            {
+                MessageBox.Show("Pole nie może być puste!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
+
