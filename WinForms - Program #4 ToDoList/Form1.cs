44734e7846c0c7cb5a4fe503f1ac2d5144f30b03
Sebastia n <KamashiVentures@gmail.com>
2025-02-24 20:53:15 +0100

    Theme added

--- WinForms - Program #4 ToDoList/Form1.cs
+++ WinForms - Program #4 ToDoList/Form1.cs
@@ -13,6 +13,55 @@ namespace WinForms___Program__4_ToDoList
 {
     public partial class Form1: Form
     {
+        private bool isDarkTheme = true;
+        void ToggleTheme()
+        {
+            if (isDarkTheme)
+            {
+                // Jasny motyw
+                this.BackColor = Color.White;
+                button1.BackColor = Color.LightGray;
+                button2.BackColor = Color.LightGray;
+                button3.BackColor = Color.LightGray;
+                button4.BackColor = Color.LightGray;
+                richTextBox1.BackColor = Color.White;
+                checkedListBox1.BackColor = Color.White;
+                textBox1.BackColor = Color.White;
+
+                // Ciemny tekst dla lepszej widoczności
+                button1.ForeColor = Color.Black;
+                button2.ForeColor = Color.Black;
+                button3.ForeColor = Color.Black;
+                button4.ForeColor = Color.Black;
+                richTextBox1.ForeColor = Color.Black;
+                checkedListBox1.ForeColor = Color.Black;
+                textBox1.ForeColor = Color.Black;
+            }
+            else
+            {
+                // Ciemny motyw
+                this.BackColor = Color.FromArgb(50, 50, 60);
+                button1.BackColor = Color.FromArgb(89, 95, 110);
+                button2.BackColor = Color.FromArgb(89, 95, 110);
+                button3.BackColor = Color.FromArgb(89, 95, 110);
+                button4.BackColor = Color.FromArgb(89, 95, 110);
+                richTextBox1.BackColor = Color.FromArgb(117, 121, 133);
+                checkedListBox1.BackColor = Color.FromArgb(117, 121, 133);
+                textBox1.BackColor = Color.FromArgb(117, 121, 133);
+
+                // Jasny tekst dla lepszej widoczności
+                button1.ForeColor = Color.White;
+                button2.ForeColor = Color.White;
+                button3.ForeColor = Color.White;
+                button4.ForeColor = Color.White;
+                richTextBox1.ForeColor = Color.White;
+                checkedListBox1.ForeColor = Color.White;
+                textBox1.ForeColor = Color.White;
+            }
+
+            isDarkTheme = !isDarkTheme;
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -42,6 +91,11 @@ namespace WinForms___Program__4_ToDoList
         private void button4_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ToggleTheme();
         }
     }
 }
