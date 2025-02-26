56835326f23d8811a4614bc49f6023ca2b0ccaeb
Sebastia n <KamashiVentures@gmail.com>
2025-02-26 10:51:45 +0100

    Calculator V2.0

--- WinForms - Program #5 Calculator V2.0/Form1.Designer.cs
+++ WinForms - Program #5 Calculator V2.0/Form1.Designer.cs
@@ -66,7 +66,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.ButtonNumber);
             // 
             // button2
             // 
