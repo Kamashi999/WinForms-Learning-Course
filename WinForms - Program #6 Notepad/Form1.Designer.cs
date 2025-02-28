b94a5a58bc55342415dfaea61bdc4f356dcb2bbb
Sebastia n <KamashiVentures@gmail.com>
2025-02-28 13:30:57 +0100

    Notepad - gui, save files

--- WinForms - Program #6 Notepad/Form1.Designer.cs
+++ WinForms - Program #6 Notepad/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(497, 540);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 546);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save it into file!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SaveFile);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(387, 546);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Load your last file!";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(226, 546);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(47, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.ExitProgram);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(499, 571);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +87,11 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- WinForms - Program #6 Notepad/Form1.cs
+++ WinForms - Program #6 Notepad/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,34 @@ namespace WinForms___Program__6_Notepad
 {
     public partial class Form1: Form
     {
+        string output;
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        void SaveFile()
+        {
+            string fileName = "myFile.txt";
+            string path = @"files\" + fileName;
+            output += richTextBox1.Text;
+
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine(output);
+                sw.Close();
+            }
+        }
+
+        private void SaveFile(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void ExitProgram(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
