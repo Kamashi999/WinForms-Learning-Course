931b006f974c9647e4eb67fcd63840e8e0aa5eef
Sebastia n <KamashiVentures@gmail.com>
2025-03-04 13:37:26 +0100

    Random Math Task - Alpha

---
+++ WinForms - Program #7 Random Math Task/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms___Program__7_Random_Math_Task
+{
+    public partial class Form1: Form
+    {
+        Random rand = new Random();
+        int a;
+        int b;
+        int randTask;
+        int result;
+        int answer;
+        public Form1()
+        {
+            InitializeComponent();
+            a = rand.Next(0,10);
+            b = rand.Next(0,10);
+            randTask = rand.Next(1, 3);
+            switch (randTask)
+            {
+                case 1:
+                    label1.Text = a.ToString() + "+" + b.ToString();
+                    answer = a + b;
+                    break;
+                case 2:
+                    label1.Text = a.ToString() + "-" + b.ToString();
+                    answer = a - b;
+                    break;
+            }
+            label2.Text = answer.ToString();
+        }
+    }
+}
---
+++ WinForms - Program #7 Random Math Task/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WinForms - Program #7 Random Math Task")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinForms - Program #7 Random Math Task")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("86627bb9-a1b5-468c-93e6-cfe8ec021aa0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
